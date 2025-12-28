e39cfd2393a062098b8f0dea2e66c79a1750d4a7
jackwhich <jackwhich@example.com>
2025-12-29 03:10:14 +0800

    修复: 更新 generate-metrics-docs.go 以匹配新的 NewJobCollector 函数签名

--- hack/generate-metrics-docs.go
+++ hack/generate-metrics-docs.go
@@ -27,7 +27,7 @@ func main() {
 
 	collectors = append(
 		collectors,
-		exporter.NewJobCollector(slog.Default(), nil, nil, nil, config.Load().Target, true, "", 0, nil).Metrics()...,
+		exporter.NewJobCollector(slog.Default(), nil, nil, nil, config.Load().Target, true, "", 0, 0, nil).Metrics()...,
 	)
 
 	metrics := make([]metric, 0)
